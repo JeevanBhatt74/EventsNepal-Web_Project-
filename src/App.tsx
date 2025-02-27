d4d74de5d7440fc5ae3b3988dcb6cd65b2b788e5
JeevanBhatt74 <iamjeevanrkb@gmail.com>
2025-02-28 01:00:35 +0545

    add pages imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import Header from "./header";
+import Blogs from "./blogs";
+import SignIn from "./sign-in";
+import SignUp from "./sign-up";
+import Home from "./home";
+import Booking from "./booking";
+import AdminDashboard from "./admin/dashboard";
+import EditEvent from "./admin/edit";
+import Profile from "./profile";
 
 export default function App() {
   return (
