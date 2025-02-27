184818f1cfe143c17af0e584fcd01f14edccf8ff
JeevanBhatt74 <iamjeevanrkb@gmail.com>
2025-02-28 01:00:17 +0545

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import { BrowserRouter,  Routes } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-       
+        <Route
+          path=""
+          element={
+            <>
+              <Header />
+              <Outlet />
+            </>
+          }
+        >
+          <Route path="/" element={<Home />} />
+          <Route path="/upcomming-events" element={<Blogs />} />
+          <Route path="/booking" element={<Booking />} />
+        </Route>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/admin">
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="edit/:eventId" element={<EditEvent />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
